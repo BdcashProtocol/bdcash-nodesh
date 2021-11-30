5c57ce45f7c1e9d967030e5fc71af804cfeec2ac
Fabianno <37354151+bdcashdev@users.noreply.github.com>
2021-11-30 16:36:40 -0300

    update lyra to bdcash

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 if [[ $1 == "-testnet" ]]
 then
     echo "Running Bdcash NodeSh in testnet mode"
-    pkill lyrad
+    pkill bdcashd
     npm run build
     npm run start:testnet
 else
     echo "Running Bdcash NodeSh in mainnet mode"
-    pkill lyrad
+    pkill bdcashd
     npm run build
     npm run start
 fi
